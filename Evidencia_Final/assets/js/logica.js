cc9a8cc1834d9e53fbf644684ac6aa7d7c741595
DanielBernal133 <82922690+DanielBernal133@users.noreply.github.com>
2021-06-30 22:43:44 -0500

    Add files via upload

---
+++ Evidencia_Final/assets/js/logica.js
@@ -0,0 +1,15 @@
+
+
+function Juego () {
+    window.location.href ="Index.html"; 
+}
+
+function Inicio (){
+    window.location.href ="Index.html";
+}
+
+
+
+function guardar_localstorage (){
+    
+}
